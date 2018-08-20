37868c330d341c893310ad4b3644735574746f55
Hayat <hayat.hhadri@gmail.com>
2018-08-20 12:19:00 +0200

    Comments for Excel Writer class

--- src/Data.java
+++ src/Data.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//package scheduling_First_Try;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
